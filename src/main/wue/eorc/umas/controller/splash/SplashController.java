ee74bdc8fbf3727a372604576f6c28bfcda86eba
Konstantin Müller <konstantinfinn.mueller@gmx.de>
2025-09-12 23:01:14 +0200

    Now also verifying Agisoft metashape version. Needs to be saved when calling settings somehow

--- src/main/wue/eorc/umas/controller/splash/SplashController.java
+++ src/main/wue/eorc/umas/controller/splash/SplashController.java
@@ -4,6 +4,7 @@ import com.mongodb.*;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import javafx.application.Platform;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.Pane;
@@ -11,16 +12,18 @@ import org.bson.BsonDocument;
 import org.bson.BsonInt64;
 import org.bson.Document;
 import org.bson.conversions.Bson;
+import wue.eorc.umas.agisoft.AgisoftCaller;
 import wue.eorc.umas.controller.scenes.main.DisplayController;
 import wue.eorc.umas.controller.scenes.views.panes.FutureController;
+import wue.eorc.umas.enums.Setting;
 import wue.eorc.umas.exception.UMASException;
 import wue.eorc.umas.loader.ProjectCache;
 import wue.eorc.umas.loader.Settings;
 import wue.eorc.umas.utils.ItemSearcher;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ScheduledFuture;
 
 public class SplashController implements FutureController {
 
@@ -28,24 +31,26 @@ public class SplashController implements FutureController {
     @Override
     public CompletableFuture<Void> init(Pane pane, DisplayController display) throws UMASException {
         Label label = ItemSearcher.getItemById("splash.label", pane, Label.class);
-        label.setText("Checking system files...");
-
         ProgressBar progressBar = ItemSearcher.getItemById("splash.progress", pane, ProgressBar.class);
-        progressBar.setProgress(0.2);
 
         return CompletableFuture.runAsync(() -> {
             try {
+                label.setText("Checking system files...");
                 ProjectCache.createRecentProjectsFile();
                 Settings.createSettingsFile();
-            } catch (IOException e) {
+                Thread.sleep(1000);
+            } catch (IOException | InterruptedException e) {
                 throw new RuntimeException(e);
             }
 
-            label.setText("Testing database connection...");
+            Platform.runLater(() -> label.setText("Testing database connection..."));
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
 
-            // Replace the placeholder with your Atlas connection string
             String uri = "mongodb://localhost:27017";
-            // Construct a ServerApi instance using the ServerApi.builder() method
             ServerApi serverApi = ServerApi.builder()
                     .version(ServerApiVersion.V1)
                     .build();
@@ -53,23 +58,50 @@ public class SplashController implements FutureController {
                     .applyConnectionString(new ConnectionString(uri))
                     .serverApi(serverApi)
                     .build();
-            // Create a new client and connect to the server
+            Platform.runLater(() -> label.setText("Built database connection string..."));
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            Platform.runLater(() -> label.setText("Trying to reach database server..."));
+
             try (MongoClient mongoClient = MongoClients.create(settings)) {
                 MongoDatabase database = mongoClient.getDatabase("flights");
                 try {
-                    // Send a ping to confirm a successful connection
                     Bson command = new BsonDocument("ping", new BsonInt64(1));
                     Document commandResult = database.runCommand(command);
-                    System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+
+                    Platform.runLater(() -> label.setText("Successfully pinged database..."));
+
+                    Thread.sleep(2000);
+
                 } catch (MongoException me) {
-                    System.err.println(me);
+                    Platform.runLater(() -> label.setText("Could not connect to database..."));
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
+            Platform.runLater(() -> label.setText("Verifying Agisoft Metashape version..."));
 
             try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                String version = new AgisoftCaller(null, null, display)
+                        .checkAgisoftVersion(Settings.getSetting(Setting.AGISOFTEXECPATH));
+                Platform.runLater(() -> label.setText("Verified: " + version));
+
+                Thread.sleep(2000);
+            } catch (InterruptedException | URISyntaxException e) {
+                Platform.runLater(() -> label.setText("Could not verify Agisoft executable..."));
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
 
         });
